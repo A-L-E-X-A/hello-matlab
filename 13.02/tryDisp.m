f901b6ebb00783b022bb48fa5b130ce9261acbea
A-L-E-X-A <47576736+A-L-E-X-A@users.noreply.github.com>
2019-02-13 10:33:22 +0300

    Directory added

---
+++ 13.02/tryDisp.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = tryDisp(inputArg1,inputArg2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = strsplit(['asdf' ' ' 'dasdsf']);
+outputArg2 = inputArg2;
+disp('hello')
+end
+
